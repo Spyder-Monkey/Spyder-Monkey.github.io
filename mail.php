c129a8160b31499c469ef8eeab366b189821632c
Trevor Bender <tbender@ptd.net>
2023-04-03 13:07:49 -0400

    contact form mail script

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "tbends@trevorbender.com";
+    $subject = "New email from portfolio site";
+    $txt = "Name: ". $name . "\r\nEmail: " . $email . "\r\nMessage: " . $message;
+    $headers = "From: noreply@trevorbender.com";
+
+    if($email!=NULL) {
+        if(mail($to, $subject, $txt, $headers)) {
+            echo "mail sent.";
+        }
+        else {
+            echo "failed";
+        }
+    }
+?>
